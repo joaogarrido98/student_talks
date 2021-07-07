45cf0e1e23165a4eeb171802aec6dd5c0f452bef
João Garrido <joao.melo.garrido@hotmail.com>
2021-07-07 02:02:00 +0100

    more json

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const recentPosts;
     fetch("https://joaogarrido98.github.io/blog/resources/posts.json")
         .then(response => response.json())
-        .then(data => console.log(data));
-
+        .then(data => loadData(data.posts));
 });
+
+function loadData(posts) {
+    let totalPosts = posts.length;
+    if (totalPosts >= 10) {
+        totalPosts = 9;
+    }
+    for (let i = 0; i < totalPosts; i++) {
+        console.log(posts[i])
+    }
+}
