00bbcaa0ebd870d0a04996446ac946095a1bf9f9
João Garrido <joao.melo.garrido@hotmail.com>
2021-07-07 16:05:10 +0100

    cards almost done

--- scripts/index.js
+++ scripts/index.js
@@ -20,6 +20,26 @@ function createItem(posts, ul) {
     let url = "https://joaogarrido98.github.io/post?id=" + posts["post_id"];
     let li = document.createElement("li");
     li.classList.add("list-item");
-    li.innerHTML = `<div class='post-item'><div class='post-inner'><a href='${url}' class='post-link'><span class='post-title'>${posts['title']}</span></a><p class='post-description'>${posts["short-description"]}</p></div></div>`;
+    li.innerHTML =
+        `<div class='post-item'>
+        <div class='post-wrapper'>
+            <div class='post-inner'>
+                <a href='${url}' class='post-link'>
+                <span class='post-title'>${posts['title']}</span>
+                </a>
+                <div class='post-description'>${posts["short-description"]}</div>
+                <div class='utility-info'>
+                <div class='left'> 
+                    <ul class='tags-list'>
+                        <li class='post-tag'>${posts['tags'][0]}</li>
+                        <li class='post-tag'>${posts['tags'][1]}</li>
+                    </ul>
+                </div>
+                <div class='right'>
+                    <span>${posts["year"]}</span><i class='fa fa-calendar'></i>
+                </div>
+            </div>
+        </div>
+    </div>`;
     ul.append(li);
 }
