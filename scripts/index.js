9f771d63f7fa9c67604c5cda28951ea785fd6026
João Garrido <joao.melo.garrido@hotmail.com>
2021-07-07 12:40:47 +0100

    improved js and json

--- scripts/index.js
+++ scripts/index.js
@@ -9,7 +9,17 @@ function loadData(posts) {
     if (totalPosts >= 10) {
         totalPosts = 10;
     }
+    let ul = document.querySelector(".posts-list");
     for (let i = 0; i < totalPosts; i++) {
-        console.log(posts[i])
+        createItem(posts[i], ul);
     }
+}
+
+
+function createItem(posts, ul) {
+    let url = "https://joaogarrido98.github.io/post?id=" + posts["post_id"];
+    let li = document.createElement("li");
+    li.classList.add("list-item");
+    li.innerHTML = `<div class='post-item'><div class='post-inner'><a href='${url}' class='post-link'><span class='post-title'>${posts['title']}</span></a><p class='posts-description'>${posts["short-description"]}</p></div></div>`;
+    ul.append(li);
 }
