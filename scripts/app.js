323d647c52ff02264988ffee3cfd172e86d7d9ff
João Garrido <joao.melo.garrido@hotmail.com>
2021-07-09 00:44:32 +0100

    nav bar fully done

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
     const hamburger = document.querySelector(".hamburger");
     const navMenu = document.querySelector(".nav-menu");
+    const element = document.body;
 
     hamburger.addEventListener("click", () => {
         if (navMenu.classList.contains("active")) {
             hamburger.classList.remove("active");
             navMenu.classList.remove("active");
+            hamburger.style.position = "static";
+            element.classList.toggle("lock-scroll");
         } else {
             hamburger.classList.add("active");
             navMenu.classList.add("active");
+            hamburger.style.position = "fixed";
+            element.classList.toggle("lock-scroll");
         }
     });
 });
