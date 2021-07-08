1917f976213bbc886334bf6697df0c2ce3b967f7
João Garrido <joao.melo.garrido@hotmail.com>
2021-07-08 01:00:10 +0100

    some improvements theme chooser

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,29 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch("https://joaogarrido98.github.io/blog/resources/posts.json")
         .then(response => response.json())
         .then(data => loadData(data.posts));
+
+    const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+    toggleSwitch.addEventListener('change', (event) => {
+        if (event.target.checked) {
+            document.documentElement.setAttribute('data-theme', 'light');
+            localStorage.setItem('theme', 'light');
+        }
+        else {
+            document.documentElement.setAttribute('data-theme', 'dark');
+            localStorage.setItem('theme', 'dark');
+        }
+    });
+
+    const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+    if (currentTheme) {
+        document.documentElement.setAttribute('data-theme', currentTheme);
+
+        if (currentTheme === 'light') {
+            toggleSwitch.checked = true;
+        }
+    }
 });
 
 function loadData(posts) {
