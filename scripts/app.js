6241f3982c91c8cd23b902271529951ae5f5d119
João Garrido <joao.melo.garrido@hotmail.com>
2021-07-09 23:13:42 +0100

    cursor pointer

--- scripts/app.js
+++ scripts/app.js
@@ -7,12 +7,10 @@ document.addEventListener("DOMContentLoaded", () => {
         if (navMenu.classList.contains("active")) {
             hamburger.classList.remove("active");
             navMenu.classList.remove("active");
-            hamburger.style.position = "static";
             element.classList.toggle("lock-scroll");
         } else {
             hamburger.classList.add("active");
             navMenu.classList.add("active");
-            hamburger.style.position = "fixed";
             element.classList.toggle("lock-scroll");
         }
     });
