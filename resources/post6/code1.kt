6a2da78732291a48c7afef3dc191f159a98b3542
João Garrido <joao.melo.garrido@gmail.com>
2022-08-12 23:26:34 +0100

    code done

--- resources/post6/code1.kt
+++ resources/post6/code1.kt
@@ -1,2 +1,22 @@
-val coins : listOf<Int>(2,1,0.50,0.20,0.10,0.05,0.02,0.01)
-val change : Double = 7.3
+// array of all the coin values
+// make sure is sorted
+val coins = listOf(25, 10, 5, 1)
+// change to be given
+var change: Int = 72
+// keep track of how many coins
+var howManyCoins: Int = 0
+
+// go through all the coins
+for (coin in coins) {
+    // check if change value is bigger than coin
+    // if it is we subtrack the value of the coin from the change
+    // and we increment the count of the coins by one
+    while (change >= coin) {
+        change -= coin
+        howManyCoins++
+    }
+    // if change is 0 we break the loop, no need to continue looping
+    if (change == 0) {
+        break
+    }
+}
